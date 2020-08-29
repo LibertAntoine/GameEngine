bdfd8a5ebdcae8291da8b9bb7516e5cf65d3e81a
AntoineLibert <duncan-arrakis@hotmail.fr>
2020-08-29 15:09:34 +0200

    Add premake system

--- GameEngine/GameEngine/src/Game/Core.h
+++ GameEngine/GameEngine/src/Game/Core.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #ifdef GE_PLATFORM_WINDOWS
-	#ifdef GE_BUILD_ALL
+	#ifdef GE_BUILD_DLL
 		#define GE_API __declspec(dllexport)
 	#else
 		#define GE_API __declspec(dllimport)
 	#endif
 #else
 	#error GameEngine only support Windows !
-#endif
+#endif 
