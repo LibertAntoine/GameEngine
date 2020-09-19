75ad1f429e47fb715b4a0057bd95b01e37f71a43
AntoineLibert <duncan-arrakis@hotmail.fr>
2020-09-19 14:20:23 +0200

    Improve Quad Rendering

--- GameEngine/GameEngine/src/Game/Imgui/ImguiLayer.cpp
+++ GameEngine/GameEngine/src/Game/Imgui/ImguiLayer.cpp
@@ -27,6 +27,8 @@ namespace GameEngine {
 
 	void ImGuiLayer::OnAttach()
 	{
+		GE_PROFILE_FUNCTION();
+
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -56,6 +58,8 @@ namespace GameEngine {
 
 	void ImGuiLayer::OnDetach()
 	{
+		GE_PROFILE_FUNCTION();
+
 		ImGui_ImplOpenGL3_Shutdown();
 		ImGui_ImplGlfw_Shutdown();
 		ImGui::DestroyContext();
@@ -63,6 +67,8 @@ namespace GameEngine {
 
 	void ImGuiLayer::Begin() 
 	{
+		GE_PROFILE_FUNCTION();
+
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
@@ -70,6 +76,8 @@ namespace GameEngine {
 
 	void ImGuiLayer::End() 
 	{
+		GE_PROFILE_FUNCTION();
+
 		ImGuiIO& io = ImGui::GetIO();
 		Application& app = Application::Get();
 		io.DisplaySize = ImVec2((float)app.GetWindow().GetWidth(), (float)app.GetWindow().GetHeight());
